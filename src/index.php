f84642da9f5193c3f2bf8b030a2c3d32c912b85b
Auriane <auriane@uccelolabs.com>
2022-02-01 21:11:57 +0100

    feat(ui): custom form upload

--- src/index.php
+++ src/index.php
@@ -8,16 +8,21 @@
     <title>Fusion d'images</title>
 </head>
 <body>
+    <h1>Photoshop Low Cost</h1>
     <form action="upload/upload.php">
-        <div>
-            <label for="file">Upload votre 1ère image</label><br>
-            <input name="file" type="file">
+        <div class="flex flex-col justify-center">
+            <div class="flex flex-row justify-center space-x-4">
+                <div class="flex flex-col">
+                    <label for="file">Upload votre 1ère image</label>
+                    <input name="file" type="file">
+                </div>
+                <div class="flex flex-col">
+                    <label for="file2">Upload votre 2e image</label>
+                    <input name="file2" type="file">
+                </div>
+            </div>
+            <button type="submit">Confirmer</button>
         </div>
-        <div>
-            <label for="file2">Upload votre 2e image</label><br>
-            <input name="file2" type="file">
-        </div>
-        <button type="submit">Confirmer</button>
     </form>
 </body>
 </html>
