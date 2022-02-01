e78fd656d3942248940bdf1c4f1d81b0e666ef84
Auriane <auriane@uccelolabs.com>
2022-02-01 21:16:49 +0100

    feat(ui): custom form upload

--- src/index.php
+++ src/index.php
@@ -7,10 +7,10 @@
     <script src="https://cdn.tailwindcss.com"></script>
     <title>Fusion d'images</title>
 </head>
-<body>
-    <h1>Photoshop Low Cost</h1>
-    <form action="upload/upload.php">
-        <div class="flex flex-col justify-center">
+<body class="m-0 p-0 w-full h-full">
+    <h1 class="p-4 text-center">Photoshop Low Cost</h1>
+    <form action="upload/upload.php" class="m-10">
+        <div class="flex flex-col justify-center space-y-10">
             <div class="flex flex-row justify-center space-x-4">
                 <div class="flex flex-col">
                     <label for="file">Upload votre 1ère image</label>
