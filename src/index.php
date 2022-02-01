dbc5a9aae5cb1ada8c21d114b95839418cabe945
Auriane <auriane@uccelolabs.com>
2022-02-01 21:21:21 +0100

    feat(ui): paragraph

--- src/index.php
+++ src/index.php
@@ -8,7 +8,10 @@
     <title>Fusion d'images</title>
 </head>
 <body class="m-0 p-0 w-full h-full">
-    <h1 class="p-4 text-center">Photoshop Low Cost</h1>
+    <div class="p-4 text-center space-y-10">
+        <h1>Photoshop Low Cost</h1>
+        <p>Télécharger vos deux photos pour les fusionner en une !</p>
+    </div>
     <form action="upload/upload.php" class="m-10">
         <div class="flex flex-col justify-center space-y-10">
             <div class="flex flex-row justify-center space-x-4">
