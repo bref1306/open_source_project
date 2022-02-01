054443613f3c1eee4ea0b4a8a382270370af24ce
Auriane <auriane@uccelolabs.com>
2022-02-01 20:23:30 +0100

    feat(ui): add input

--- src/index.php
+++ src/index.php
@@ -1,4 +1,21 @@
-<?php 
-
-
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Fusion d'images</title>
+</head>
+<body>
+    <form action="">
+        <div>
+            <label for="file">Upload votre 1ère image</label><br>
+            <input name="file" type="file">
+        </div>
+        <div>
+            <label for="file2">Upload votre 2e image</label><br>
+            <input name="file2" type="file">
+        </div>
+    </form>
+</body>
+</html>
