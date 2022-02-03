965d080b818570643bc1167c3f07ac64ca66f035
luc-lecoutour <77966986+luc-lecoutour@users.noreply.github.com>
2022-02-03 12:09:03 +0100

    fix :apercue avant fusion

--- src/upload/resize.php
+++ src/upload/resize.php
@@ -1,91 +1,109 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
 
 <div style="float: left;width: 400px">
-    <div>
-        <form id="formulaireImg">
+    <form id="formulaireImg">
+        <div>
             <label>Largueur image principale</label>
-            <input id="image1" value="" name="image1" onchange="setPosImg(this.value)" placeholder="initial : 500px"><br>
+            <input id="image1" value="500" name="widthPrimary" onchange="setPosImg(this.value)" placeholder="initial : 500px"><br>
+            <input type="hidden" id="HeightImgPrimary" value="0" name="HeightImgPrimary">
             <label>Largueur image à superposer</label>
-            <input id="image2" value="" name="image2" onchange="setPosImgSuperpose(this.value)" placeholder="initial : 50px">
-            <input type="hidden" id="apercuHeight" value="" name="apercuHeight">
-        </form>
-    </div>
+            <input id="image2" value="50" name="widthSecondary" onchange="setPosImgSuperpose(this.value)" placeholder="initial : 50px">
+            <input type="hidden" id="HeightImgSecondary" value="0" name="HeightImgSecondary">
+        </div>
 
-    <div style="margin-top: 50px">
-        <h2>Position 2e image à superposer</h2>
-        <label>positionX</label>
-        <input type="number" min="0" id="largeur" value="" onchange="getPosX()" placeholder="initial : 0"><br>
-        <label>positionY</label>
-        <input type="number" min="0" id="hauteur" value="" onchange="getPosX()" placeholder="initial : 0">
-    </div>
+        <div style="margin-top: 50px">
+            <h2>Position 2e image à superposer</h2>
+            <label>positionX</label>
+            <input type="number" min="0" id="largeur" name="posX" value="0" onchange="setPosImgSecondary()" placeholder="initial : 0"><br>
+            <label>positionY</label>
+            <input type="number" min="0" id="hauteur" value="0" name="posY" onchange="setPosImgSecondary()" placeholder="initial : 0">
+
+
+
+        </div>
+    </form>
 </div>
 
-<div  style="float: left;">
-    <div style="position: relative;border: 1px solid red;width: 500px" id="apercu">
-        <img src="image/fichier1" id="img1"style="width: 100%">
-        <img src="image/copy" id="img2" style="width: 50px;position: absolute;top: 0;left:0">
-    </div>
-    <button id="sauvegarder" style="margin-top: 100px">Télécharger</button>
-    <div id="messageResponse">
+    <div  style="float: left;">
+        <div style="position: relative;border: 1px solid red;width: 500px" id="apercu">
+            <img src="image/fichier1" id="img1"style="width: 100%">
+            <img src="image/fichier2" id="img2" style="width: 50px;position: absolute;top: 0;left:0">
+        </div>
+        <button id="sauvegarder" style="margin-top: 100px">Télécharger</button>
+        <div id="messageResponse">
 
+        </div>
     </div>
-</div>
 
 
 <script>
+    //set input hidden for both images heigth
+    function setHeight(){
+        let element1=document.getElementById("img1").clientHeight;
+        let element2 = document.getElementById("img2").clientHeight;
+
+        jQuery('#HeightImgPrimary').val(element1);
+        jQuery('#HeightImgSecondary').val(element2);
+    }
+
+    //display primary image width  onchange
     function setPosImg(value){
-        if(value!=0){
+        if(value>0){
             jQuery('#img1').css('width',value);
             jQuery('#apercu').css('width',value);
         }
+        else{
+            jQuery('#image1').val(document.getElementById("img1").clientWidth);
+        }
+
 
     }
-
+    //display secondary  image width  onchange
     function setPosImgSuperpose(value){
-        if(value!=0)
+        if(value>0){
             jQuery('#img2').css('width',value);
+        }
+        else{
+            jQuery('#image2').val(document.getElementById("img2").clientWidth);
+        }
+
     }
 
     //sauvegarder image TODO
     jQuery("#sauvegarder").click(()=>{
 
-        jQuery.post("createImage.php",jQuery("#formulaireImg").serialize(), function(data){
-            //jQuery("#apercu").css('width',data+"px");
-            console.log(data);
-        })
+        //TODO controle si image bien positionné et non en dehors du cadre
+        if(jQuery("#largeur").val()>=0 && jQuery("#hauteur").val()>=0){
+            setHeight();
+            jQuery.post("createImage.php",jQuery("#formulaireImg").serialize(), function(data){
+                //jQuery("#apercu").css('width',data+"px");
+                console.log(data);
+            },'json');
+        }
+        else{
+
+        }
     })
 
 </script>
 
 <script>
-    let largeur=document.getElementById("largeur");
-    let hauteur=document.getElementById("hauteur");
-    let btn=document.getElementById("validate");
-    let img=document.getElementById("img2");
+    function setPosImgSecondary() {
+        let posX = document.getElementById("largeur");
+        let posY = document.getElementById("hauteur");
 
-    //default image position
-    //img.style+="position: absolute;top:0; left:0";
-
-
-    function getPosX(){
-        let posX=document.getElementById("largeur");
-        let posY=document.getElementById("hauteur");
-
-        if(posX.value!="" && posY.value !="" && (posX.value >=0 && posY.value>=0) ){
-            if(posX.value==""){
-                posX.value=0;
+        if ((posX.value === "") || (posY.value === "") || (posX.value >= 0 && posY.value >= 0)) {
+            if (posX.value == "") {
+                posX.value = 0;
             }
-            if(posY.value==""){
-                posY.value=0;
+            if (posY.value == "") {
+                posY.value = 0;
             }
-            else{
-                //img.style+="position: absolute; top:"+posX.value+"px; left:"+posY.value+"px";
-                jQuery("#img2").css("top",posX.value);
-                jQuery("#img2").css("left",posY.value);
-            }
+            jQuery("#img2").css("top", posX.value);
+            jQuery("#img2").css("left", posY.value);
         }
-
     }
+
 </script>
 
 <?php
