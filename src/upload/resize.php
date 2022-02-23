005af05d678e41a61a4ae54e3e8990f913148eaa
Auriane <auriane@uccelolabs.com>
2022-02-23 15:56:24 +0100

    feat(ui): resize

--- src/upload/resize.php
+++ src/upload/resize.php
@@ -12,9 +12,9 @@
 <body class="m-0 p-0 w-full h-full bg-stone-200">
 <?php include('../components/header.php')?>
 <div class="p-10">
-    <div class="flex flex-col-reverse md:flex-row md:space-x-16">
+    <div class="flex flex-col-reverse md:flex-row md:space-x-16 items-start">
         <div class="flex w-full justify-center items-center md:w-1/2 md:mb-0 bg-white px-2 pb-2 rounded-xl mt-6">
-            <div class="flex h-full flex-col justify-between items-center my-6">
+            <div class="flex h-full flex-col justify-between items-center mb-6">
                 <div class="flex flex-row rounded-xl space-x-3 p-2 w-max items-center">
                     <div class="rounded-full bg-white p-2">
                         <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
