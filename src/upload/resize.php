18b7efe7bcadd9aa3e3c5ba90c91bc3c4853061f
Auriane <auriane@uccelolabs.com>
2022-02-23 14:30:34 +0100

    feat: update resize

--- src/upload/resize.php
+++ src/upload/resize.php
@@ -26,7 +26,7 @@
                 </div>
                 <div class="relative" id="apercu">
                     <?php require '../Fusion.php'; ?>
-                    <img src="<?php echo Fusion::getPathFile("fichier1")?>" id="img1" style="width: 100%">
+                    <img src="<?php echo Fusion::getPathFile("fichier1")?>" id="img1">
                     <img src="<?php echo Fusion::getPathFile("fichier2")?>" id="img2" style="width: 50px;position: absolute;top: 0;left:0">
                 </div>
                 <div id="messageResponse"></div>
@@ -80,7 +80,8 @@
             <button id="sauvegarder" class="flex flex-row space-x-2 bg-black hover:bg-red-700 text-white text-sm px-4 py-2 border rounded-full">
                 <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z" />
-                </svg>   
+                </svg>
+                <p class="text-base">Télécharger</p>   
             </button>
         </div>
         
